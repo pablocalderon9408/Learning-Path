f5b0fbde918526883dc4fd1cd30fedecdc700cb0
Pablo Calderon <pablocalderon9408@gmail.com>
2022-07-22 13:16:25 -0500

    sets

---
+++ Backend_Development_Python_Django/python_intermediate_lambdas/manejo_fechas.py
@@ -0,0 +1,17 @@
+import datetime
+
+my_time = datetime.datetime.now()
+my_time = datetime.datetime.utcnow()
+my_day = datetime.date.today()
+
+print(f"Year: {my_day.year}")
+print(f"Month: {my_day.month}")
+print(f"Day: {my_day.day}")
+
+"%Y = YEAR"
+"%m = MONTH"
+"%d = DAY"
+"%H = HOUR"
+"%M = MINUTE"
+"%S = SECOND"
+
