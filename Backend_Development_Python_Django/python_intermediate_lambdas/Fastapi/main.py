88465915793ec214fec770a676d3bedc61501d28
Pablo Calderon <pablocalderon9408@gmail.com>
2022-07-27 09:11:00 -0500

    path and body parameters

--- Backend_Development_Python_Django/python_intermediate_lambdas/Fastapi/main.py
+++ Backend_Development_Python_Django/python_intermediate_lambdas/Fastapi/main.py
@@ -5,11 +5,17 @@ from typing import Optional
 from pydantic import BaseModel
 
 # FastAPI
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Path, Query
 
 app = FastAPI()
 
 # Models
+class Location(BaseModel):
+    city: str
+    state: str
+    country: str
+
+
 
 class Person(BaseModel):
     first_name: str
@@ -26,3 +32,55 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+
+# Los query parameters son aquellos que son opcionales, vienen después del ? en la url.
+@app.get("/person/detail")
+def show_person(
+    # Name es opcional y es un Query parameter. En caso tal de que lo envíen, debe cumplir la longitud.
+    name: Optional[str] = Query(
+        None, min_length=1, 
+        max_length=50,
+        title="Person Name",
+        description="This is the person name. 1 to 50 characters."
+        ),
+    # El query parameter en este caso es obligatorio, pero no es una buena práctica.
+    # Si es obligatorio, debería ser un path parameter.
+    age: str = Query(
+        ...,
+        title="Person age",
+        description="This is the person age. Required."
+        )
+):
+    return {name:age}
+
+
+@app.get("/person/detail/{person_id}")
+def show_person(
+    # Los path parameters son obligatorios.
+    person_id: int = Path(..., gt=0)
+
+):
+    return {person_id: "It exists!"}
+
+
+# Validaciones de Request Body.
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path(
+        ...,
+        title="Person id",
+        description="This is the person id.",
+        gt=0
+    ),
+    person: Person = Body(
+        ...
+        ),
+    location: Location = Body(
+        ...
+    )
+):
+    results = person.dict()
+    results.update(location.dict())  
+    return results
---
+++ Backend_Development_Python_Django/python_intermediate_lambdas/bairesdev.py
@@ -0,0 +1,6 @@
+def a():
+    a()
+    return 42
+
+if __name__ == "__main__":
+    a()
