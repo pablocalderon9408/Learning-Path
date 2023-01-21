5cbac3317fd84aa1c305046a149f15536b41e0c9
Pablo Calderon <pablocalderon9408@gmail.com>
2023-01-21 12:03:29 -0500

    functional game in same browser

---
+++ Backend_Development_Python_Django/python_intermediate_lambdas/websockets/main.js
@@ -0,0 +1,61 @@
+import { createBoard, playMove } from "./connect4.js";
+
+// const websocket = new WebSocket("ws://localhost:8001/");
+// const event = {type: "play", column: 3};
+// websocket.send(JSON.stringify(event));
+
+
+
+function sendMoves(board, websocket) {
+  // When clicking a column, send a "play" event for a move in that column.
+  console.log("como fue que como fue")
+  board.addEventListener("click", ({ target }) => {
+    const column = target.dataset.column;
+    console.log('oeeee que mas')
+    // Ignore clicks outside a column.
+    if (column === undefined) {
+      return;
+    }
+    const event = {
+      type: "play",
+      column: parseInt(column, 10),
+    };
+    websocket.send(JSON.stringify(event));
+  });
+}
+
+function showMessage(message) {
+  window.setTimeout(() => window.alert(message), 50);
+}
+
+function receiveMoves(board, websocket) {
+  websocket.addEventListener("message", ({ data }) => {
+    const event = JSON.parse(data);
+    switch (event.type) {
+      case "play":
+        // Update the UI with the move.
+        playMove(board, event.player, event.column, event.row);
+        break;
+        case "win":
+          showMessage(`Player ${event.player} wins!`);
+          // No further messages are expected; close the WebSocket connection.
+          websocket.close(1000);
+          break;
+          case "error":
+            showMessage(event.message);
+            break;
+            default:
+              throw new Error(`Unsupported event type: ${event.type}.`);
+            }
+          });
+        }
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Initialize the UI.
+  const board = document.querySelector(".board");
+  createBoard(board);
+  const websocket = new WebSocket("ws://localhost:8001/");
+  receiveMoves(board, websocket);
+  sendMoves(board, websocket);
+});
